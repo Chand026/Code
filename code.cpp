a96aee71ce6af91cc7cd16334a2459ad7ab4896e
Chand026 <134709269+Chand026@users.noreply.github.com>
2023-05-26 18:32:44 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+void fun(int row){
+   int n = 2 * row - 1;
+   int left = 0, right = n - 1;
+   vector<char> arr(n, '*');
+   for(int i = 1; i <= row; i++)
+   {
+        for(int j = 0; j < n; j++)
+        cout<<arr[j];
+        cout<<"\n";
+       arr[left] = ' ';
+       arr[right] = ' ';
+       left++;
+       right--;
+   }
+}
+
+int main(){
+    int row;
+    cin>>row;
+    
+      fun(row);
+    return 0;
+    
+}
